b79c51e2a1fd38dddc47bb26d02d06df02ef3f99
Wylie Zhao <wzhao830@gmail.com>
2021-06-05 17:01:44 -0400

    update to use services deployed on ec2

--- server/index.js
+++ server/index.js
@@ -4,48 +4,13 @@ const path = require('path');
 const app = express();
 const port = 2000;
 const proxy = httpProxy.createProxyServer({});
-const carsonService = 'http://localhost:2002';
-const yosepService = 'http://localhost:5003';
-const wylieService = 'http://localhost:2004';
+const wylieService = 'http://18.117.79.8:2004';
 
-// app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/rooms/:id', function(req, res) {
   res.render('index', { title: 'Airbnb Item Page', roomId: req.params.id });
-});
-
-//carsonService
-app.get('/calendarwidget.js', function(req, res) {
-  console.log('Request carsonService', req.method, req.url);
-  proxy.web(req, res, {target: carsonService});
-});
-
-app.get('/listings/:listingId', function(req, res) {
-  console.log('Request carsonService', req.method, req.url);
-  proxy.web(req, res, {target: carsonService});
-});
-
-app.get('/reviews/:listingId', function(req, res) {
-  console.log('Request carsonService', req.method, req.url);
-  proxy.web(req, res, {target: carsonService});
-});
-
-app.get('/bookings/:listingId', function(req, res) {
-  console.log('Request carsonService', req.method, req.url);
-  proxy.web(req, res, {target: carsonService});
-});
-
-//yosepService
-app.get('/hostedBy.js', function(req, res) {
-  console.log('Request yosepService', req.method, req.url);
-  proxy.web(req, res, {target: yosepService});
-});
-
-app.all('/api/hostedBy/:id', function(req, res) {
-  console.log('Request yosepService', req.method, req.url);
-  proxy.web(req, res, {target: yosepService});
 });
 
 //wylieService
